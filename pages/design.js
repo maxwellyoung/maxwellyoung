87e8c3b3036da8d8bb306ef9ca1babbde5975fa4
maxwellyoung <maxtheyoung@gmail.com>
2023-02-01 14:32:05 +1300

    design text styling

--- pages/design.js
+++ pages/design.js
@@ -15,27 +15,34 @@ const design = () => {
 					thought I'd include it here.
 				</p>
 			</div>
-			<div className="mb-4">
-				<p className="italic">Church - Orange Cover Artwork</p>
-				<a href="https://open.spotify.com/album/6Fg8W91ZK9HeBZSsLWXTje?si=c9245164aa614844">
-					<Image
-						src="/images/orange.png"
-						width={800}
-						height={800}
-						alt="artwork for orange by church"
-					/>
-				</a>
-			</div>
-			<div className="mb-4">
-				<p className="italic">Maxwell Young - No. 5 Cover Artwork</p>
-				<a href="https://open.spotify.com/track/0Qxq1Nf297aBK1k8E9py5P?si=2e1b028407df470f">
-					<Image
-						src="/images/no5.jpg"
-						width={800}
-						height={800}
-						alt="artwork for no. 5 by maxwell young"
-					/>
-				</a>
+			<div className="italic text-lg">
+				<div className="mb-4">
+					<p>
+						<span className="font-bold">Church</span> - Orange Cover Artwork
+					</p>
+					<a href="https://open.spotify.com/album/6Fg8W91ZK9HeBZSsLWXTje?si=c9245164aa614844">
+						<Image
+							src="/images/orange.png"
+							width={800}
+							height={800}
+							alt="artwork for orange by church"
+						/>
+					</a>
+				</div>
+				<div className="mb-4">
+					<p>
+						<span className="font-bold">Maxwell Young</span> - No. 5 Cover
+						Artwork
+					</p>
+					<a href="https://open.spotify.com/track/0Qxq1Nf297aBK1k8E9py5P?si=2e1b028407df470f">
+						<Image
+							src="/images/no5.jpg"
+							width={800}
+							height={800}
+							alt="artwork for no. 5 by maxwell young"
+						/>
+					</a>
+				</div>
 			</div>
 		</Layout>
 	)
