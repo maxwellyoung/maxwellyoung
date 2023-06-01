6fa41215b91336221a76c5faa2e1a7999ad9de56
maxwellyoung <maxtheyoung@gmail.com>
2023-06-02 09:23:03 +1200

    another type error

--- src/lib/gtm.ts
+++ src/lib/gtm.ts
@@ -1,6 +1,13 @@
 export const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID
 
+declare global {
+	interface Window {
+		dataLayer: any[] // Adjust the type as per your dataLayer structure
+	}
+}
+
 export const pageview = (url: string) => {
+	window.dataLayer = window.dataLayer || []
 	window.dataLayer.push({
 		event: 'pageview',
 		page: url,
