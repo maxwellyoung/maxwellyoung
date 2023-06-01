9f03d857119c3fafb077700c07074313c150bca3
maxwellyoung <maxtheyoung@gmail.com>
2023-06-02 11:53:45 +1200

    update about

--- src/components/About/data.ts
+++ src/components/About/data.ts
@@ -12,10 +12,10 @@ export const about: Content[] = [
 			'I primarily work with React and Next.js ecosystem with TypeScript, TailwindCSS for styling, Figma for design and a variety of database solutions. The web is an endless medium of opportunities, and I have yet to scratch the surface.',
 	},
 	{
-		title: 'Now',
+		title: 'Resume',
 		href: '/resume.pdf',
 		content:
-			'Freelancing web projects, looking for a new position to gain further experience, understanding. Click to view my resume.',
+			'As a freelance web developer, I am actively seeking new opportunities to expand my experience and deepen my understanding in order to take my skills to the next level.',
 	},
 ]
 
@@ -24,6 +24,7 @@ export const work: Content[] = [
 		title: 'Internal Dashboard App at Spark New Zealand',
 		content:
 			"During my employment at Spark New Zealand, I was tasked with designing and developing the front end of on an internal dashboard application for marketers at Spark New Zealand, and I am particularly proud of my contributions to this project. My role was to design the application in Figma based on an existing PowerBI dashboard, allowing for the inclusion of new helpful information to be displayed from machine learning algorithms and other sources. I also developed the front-end of the application in React and Next.js. Throughout the project, I collaborated closely with the product team and stakeholders to ensure that the application met their needs and was easy to use. I also implemented best practices in software development to ensure that the application was scalable, efficient, and maintainable. Overall, I am proud of my contributions to this project and the positive impact it has had on the productivity and effectiveness of Spark's marketing team.",
+		newTab: true,
 	},
 	{
 		title: 'Tom Crampin Portfolio',
@@ -42,7 +43,13 @@ export const work: Content[] = [
 	{
 		title: 'Real Time Messaging App (WIP)',
 		content:
-			"This week, I am developing a Real Time messaging app to explore the capabilities of web sockets and real-time communication. I am particularly thrilled about this project because it provides an opportunity to leverage Next.js's new App Router, which I believe represents the future direction of web development, blending server and client components seamlessly. For the implementation, I am utilizing TypeScript, React, Next.js, and Tailwind CSS. Additionally, I am integrating Socket.io for web socket functionality, and I am excited to use the Chakra UI component library for the first time, enhancing the app's user interface.",
+			"I am developing a Real Time messaging app to explore the capabilities of web sockets and real-time communication. I am particularly thrilled about this project because it provides an opportunity to leverage Next.js's new App Router, which I believe represents the future direction of web development, blending server and client components seamlessly. For the implementation, I am utilizing TypeScript, React, Next.js, and Tailwind CSS. Additionally, I am integrating Socket.io for web socket functionality, and I am excited to use the Chakra UI component library for the first time, enhancing the app's user interface.",
+		newTab: true,
+	},
+	{
+		title: 'Calender React Native App (WIP)',
+		content:
+			'Building a minimalist and efficient calendar app built with React Native and Expo, designed to simplify schedule management for developers and enhance productivity seamlessly.',
 		newTab: true,
 	},
 ]
@@ -54,8 +61,9 @@ export const writing: Content[] = [
 		href: '/writing/concurrency',
 	},
 	{
-		title: 'About',
-		content: 'Why I made this blog',
-		href: '/writing/about',
+		title: 'AI in Development',
+		content:
+			'Exploring the evolving relationship between developers and AI, this article delves into the challenges, benefits, and limitations of AI tools in the development process while emphasizing the importance of complementing AI with deep expertise and continuous learning.',
+		href: '/writing/ai',
 	},
 ]
