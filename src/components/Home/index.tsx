8d7edb88c7973359a20a285b7744fba0559f6f43
Maxwell Young <maxtheyoung@gmail.com>
2023-11-14 17:54:44 +1300

    Update index.tsx

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -33,8 +33,11 @@ const Home: FC = () => {
 					</p> */}
 					<p>
 						Hey there, I'm Maxwell Young, a CS undergrad at Auckland University of Technology. 
+						
 						I've been a UI Developer at Spark New Zealand and graduated from Dev Academy Aotearoa.
+						
 						I'm passionate about making software simple with intention and principles to elevate user experiences and solve everyday issues.
+						
 						I've also been releasing music under my own name since 2015.
 					</p>
 					
